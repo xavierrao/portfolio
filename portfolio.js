3fffadb3e09b15eb90556969db9194902d160dc4
XavierRao <xavierrao23@gmail.com>
2025-01-15 18:20:46 -0500

    Layout, still need to add descriptive info

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+// // Back to top arrow button
+
+// const backToTopButton = $("#backToTopButton");
+
+// $(window).scroll(function () {
+//   if ($(window).scrollTop() > 300) {
+//     backToTopButton.addClass("show");
+//   } else {
+//     backToTopButton.removeClass("show");
+//   }
+// });
+
+// backToTopButton.on("click", function (e) {
+//   e.preventDefault();
+//   $("html, body").animate({ scrollTop: 0 }, "300");
+// });
