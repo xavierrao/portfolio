f844a3f1814bb6992396a4e1573a6a47faccea5b
XavierRao <xavierrao23@gmail.com>
2025-01-16 03:10:15 -0500

    Auto updating navbar + figuring out a color scheme

--- portfolio.js
+++ portfolio.js
@@ -14,3 +14,29 @@ backToTopButton.on("click", function (e) {
   e.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
+
+
+//Updating NavBar
+
+const sections = document.querySelectorAll("section[id].section");
+
+function scrollTracker() {
+    const currentYScroll = window.scrollY;
+
+    sections.forEach((section) => {
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 100;
+        const id = section.getAttribute("id");
+        const currentNavLink = document.querySelector(`.nav-links a[href*="#${id}"]`);
+        if(
+            currentYScroll > sectionTop &&
+            currentYScroll <= sectionTop + sectionHeight
+        ) {
+            currentNavLink.classList.add("active");
+        } else {
+            currentNavLink.classList.remove("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollTracker);
