25995069fde5b5f8dac8baa3a40c748dcbed7e59
XavierRao <xavierrao23@gmail.com>
2025-01-16 09:43:47 -0500

    Better Spacing + Footer

--- portfolio.js
+++ portfolio.js
@@ -25,7 +25,7 @@ function scrollTracker() {
 
     sections.forEach((section) => {
         const sectionHeight = section.offsetHeight;
-        const sectionTop = section.offsetTop - 100;
+        const sectionTop = section.offsetTop - 300;
         const id = section.getAttribute("id");
         const currentNavLink = document.querySelector(`.nav-links a[href*="#${id}"]`);
         if(
